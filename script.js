5fee87ab890f3a54a4c9f2fcb8625c3e131c6605
LRaszka <Raszka.Lukas123@gmail.com>
2021-08-22 23:58:03 +0200

    done

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ const userList = [];
 getData();
 
 input.addEventListener("input", function(e){ //e je event
-    console.log(e.target.value);
+    dataFilter(e.target.value);
 });
 
-async function getData(){ //async - asynchronní JS
+async function getData(){ //async - asynchronní JS - načítá na pozadí 
+    const allUsers = await fetch("https://randomuser.me/api?results=50"); //fetch - stahuje, await - počkej než se to dokonči
+    const data = await allUsers.json(); //převede data z JSON
+    result.innerHTML = ""; //vymaže všechny li
+    data.results.forEach(user => { //práce s daty - projede se to cyklem; uloží každého uživatele do user
+        const li = document.createElement("li"); //vytvoření prázného li
+        li.innerHTML = `<img src="${user.picture.large}" alt="${user.name.first}">
+        <div class="user-information">
+            <h3>${user.name.first} ${user.name.last}</h3>
+            <p>${user.location.city}, ${user.location.country}</p>
+        </div>`
 
+        result.appendChild(li);
+
+        userList.push(li);
+    }); 
 };
+
+function dataFilter(inputText){
+    userList.forEach(oneUser => {
+        if (oneUser.innerHTML.toLowerCase().includes(inputText.toLowerCase())){
+            oneUser.classList.remove("hide");
+        } else {
+            oneUser.classList.add("hide");
+        }
+    })
+}
